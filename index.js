029ec69f118a6a39ab183ab458df919011e4587d
brandon corey <bcorey85@gmail.com>
2019-03-29 01:33:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
-  return("Happy holidays, ${name}!")
+  return(`Happy holidays, ${name}!`)
 }
 
 function happyHolidayTo(holiday, name){
