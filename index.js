e04e0219505d194c928f052bbea7412f11bd1cc5
brandon corey <bcorey85@gmail.com>
2019-03-29 01:33:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ function happyHolidayTo(holiday, name){
 }
 
 function holidayCountdown(holiday, days){
-  return(`It's ${days} days until %{holiday}!`)
+  return(`It\'s ${days} days until %{holiday}!`)
 }
